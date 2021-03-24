f40eda2c6f9ba9fb09ad7b173454a678e1e9a1aa
therealfra <francesco_volpato@studenti.naba.it>
2021-03-24 17:04:06 +0100

    script controllo cam

---
+++ Assets/Scripts/Input/KeyboardInputManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardInputManager : InputManager
+{
+    //EVENTS
+
+    public static event MoveInputHandler OnMoveInput;
+    public static event RotateInputHandler OnRotateInput;
+    public static event ZoomInputHandler OnZoomInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //movement
+        if (Input.GetKey(KeyCode.W))
+        {
+            OnMoveInput?.Invoke(Vector3.forward);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            OnMoveInput?.Invoke(-Vector3.forward);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            OnMoveInput?.Invoke(-Vector3.right);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            OnMoveInput?.Invoke(Vector3.forward);
+        }
+
+        //rotation
+        if (Input.GetKey(KeyCode.E))
+        {
+            OnRotateInput?.Invoke(-1f);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            OnRotateInput?.Invoke(1f);
+        }
+
+        //zoom
+        if (Input.GetKey(KeyCode.Z))
+        {
+            OnZoomInput?.Invoke(-1f);
+        }
+        if (Input.GetKey(KeyCode.X))
+        {
+            
+            OnZoomInput?.Invoke(1f);
+        }
+    }
+}
